ae694cc95f70768ed1d7ae84eb3d99175d7bc5da
Volodymyr <malones.vladimir@gmail.com>
2020-03-20 16:47:16 +0100

    loads all files of the app

---
+++ js/app.js
@@ -0,0 +1,98 @@
+const KEY_ENTER = 13;
+
+const form = document.querySelector('.main-form');
+const textToDetectInput = form.querySelector('.main-form__text-enter');
+const detectBtn = form.querySelector('.main-form__btn');
+const resultsOutput = document.querySelector('.app__results');
+
+// get data from server
+
+getResponse = async text => {
+  const endpoint = 'http://api.languagelayer.com/detect?access_key=';
+  const apiKey = '050e75323122ab7c32092011df465e4c';
+  const query = `&query=${text}`;
+  
+  const response = await fetch(endpoint + apiKey + query);
+  const data = await response.json();
+
+  return data;
+}
+
+// check input field
+
+checkAnyTextInTextarea = () => {  
+  if (textToDetectInput.value.length > 0) {
+    detectBtn.removeAttribute('disabled');
+    detectBtn.classList.remove('main-form__btn--disabled');
+  }
+  if (textToDetectInput.value.length === 0) {
+    detectBtn.setAttribute('disabled', 'disabled');
+    detectBtn.classList.add('main-form__btn--disabled');
+  }
+}
+
+// show results
+
+showResults = data => {
+  resultsOutput.innerHTML = '';
+
+  data.results.forEach(element => {
+    const languageName = element.language_name;
+    const probability = element.percentage;
+
+    const html = `
+    <ul class="app__output output">
+      <li class="output__language">${languageName}</li>
+      <li class="output__match">Match: ${Math.round(probability)} %</li>
+    </ul>
+  `;
+
+    resultsOutput.innerHTML += html;
+
+    const resultField = document.querySelectorAll('.output');
+    resultField.forEach(element => {
+      if (Math.round(probability) === 100) {
+        element.classList.add('output--match-gb');
+      }
+    });
+  });
+}
+
+// disable btn
+
+detectBtn.setAttribute('disabled', 'disabled');
+detectBtn.classList.add('main-form__btn--disabled');
+
+textToDetectInput.addEventListener('input', () => {
+  checkAnyTextInTextarea();
+  textToDetectInput.placeholder = textToDetectInput.value;
+});
+
+// listen to the click on button
+
+detectBtn.addEventListener('click', e => {
+  e.preventDefault();
+
+  getResponse(textToDetectInput.value.trim())
+    .then(data => showResults(data))
+    .then(err => console.log(err));
+
+  form.reset();
+  detectBtn.setAttribute('disabled', 'disabled');
+  detectBtn.classList.add('main-form__btn--disabled');
+});
+
+// listen to the press the key enter
+
+document.addEventListener('keydown', e => {
+  if (e.keyCode === KEY_ENTER) {
+    e.preventDefault();
+    getResponse(textToDetectInput.value.trim())
+    .then(data => showResults(data))
+    .then(err => console.log(err));
+
+    form.reset();
+    detectBtn.setAttribute('disabled', 'disabled');
+    detectBtn.classList.add('main-form__btn--disabled');
+  }
+});
